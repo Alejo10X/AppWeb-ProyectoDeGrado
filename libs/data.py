e05d35915f22c15e2bb875d438c704b8debe7abe
Alejo Castañeda <facastaneda@uniboyaca.edu.co>
2021-06-06 01:37:58 -0500

    4.3.3 Se mejora la funcionalidad del código en el mapa interactivo, se muestran las mediciones en las tarjetas de resumen estadístico, y se completa el correcto diseño del reporte generado, añadiendo más información importante como las tablas del análisis

--- libs/data.py
+++ libs/data.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 import folium as fm
+import branca as br
 
 def dataAnalysis(datafile):
 
@@ -27,12 +29,13 @@ def dataAnalysis(datafile):
 
     analysis.update({'A': 'Se analizaron {} mediciones'.format(len_after)})
 
+    analysis.update({'A': 'Se analizaron {} mediciones'.format(len_after)})
+
     if len_before == len_after:
-        analysis.update({'B': 'Los datos cumplen con los rangos de medición estándar de las sondas'})
+        analysis.update({'B': 'todas cumplen con los rangos de medición de cada una de las sondas implementadas en el sistema.'})
 
     else:
-        analysis.update({'B': 'Se eliminaron {} datos que no cumplen con los rangos de medición de las sondas'.format(
-            len_before - len_after)})
+        analysis.update({'B': 'se eliminaron {} que no cumplen con los rangos de medición de cada una de las sondas implementadas en el sistema.'.format(len_before - len_after)})
 
     # ANCHOR - Análisis Estadístico
 
@@ -49,6 +52,13 @@ def dataAnalysis(datafile):
         data[col] = values
 
     quickTab = pd.DataFrame(data, index=['Max', 'Min', 'Med', 'Mdna', 'Desv.Est'])
+
+    try:
+        quickTab = quickTab.rename_axis(['Estadísticas']).reset_index()
+        quickTab['Estadísticas'] = np.where(quickTab['Estadísticas'].duplicated(), '', quickTab['Estadísticas'])
+
+    except:
+        None
 
     analysis.update({'C': quickTab})
 
@@ -109,12 +119,26 @@ def dataAnalysis(datafile):
             tabCount.values[row, col] == tabCount.values[row, col] * 100 / len_after
             i += 1
 
+    try:
+        tabCount = tabCount.rename_axis(['Calificación']).reset_index()
+        tabCount['Calificación'] = np.where(tabCount['Calificación'].duplicated(), '', tabCount['Calificación'])
+
+    except:
+        None
+
     analysis.update({'E': tabCount})
 
     # ANCHOR - Porcentaje de cumplimiento total
 
     tabTotal = pd.DataFrame(tabVerif['Cumplimiento'].value_counts(normalize=True) * 100)
     tabTotal.columns = ['% Cumplimiento Total']
+
+    try:
+        tabTotal = tabTotal.rename_axis(['Calificación']).reset_index()
+        tabTotal['Calificación'] = np.where(tabTotal['Calificación'].duplicated(), '', tabTotal['Calificación'])
+
+    except:
+        None
 
     analysis.update({'F': tabTotal})
 
@@ -123,57 +147,117 @@ def dataAnalysis(datafile):
     lat = df['Lat'].tolist()
     lng = df['Lng'].tolist()
 
-    # i = 0
-    # for dato in lat:
-    #     dec, ent = math.modf(dato)
-    #     if dec < 0.76:
-    #         num = ent + (dec + (0.76 - round(dec, 2)))
-    #         lat[i] = num
-    #     i += 1
-    #
-    # i = 0
-    # for dato in lng:
-    #     dec, ent = math.modf(dato)
-    #     if abs(dec) > 0.109:
-    #         num = ent + (dec + (abs(dec) - 0.109))
-    #         lng[i] = num
-    #     i += 1
+    legend = br.element.MacroElement()
+    legend._template = br.element.Template(
+        '''
+        {% macro html(this, kwargs) %}
+
+        <div style="
+            position: fixed;
+            bottom: 10px;
+            left: 10px;
+            width: 180px;
+            height: 66px;
+            z-index:9998;
+            font-size:14px;
+            background-color: #ffffff;
+            opacity: 1;
+            border-radius: 3px;
+            box-shadow: 0 1px 5px rgb(0 0 0 / 65%);
+            ">
+
+            <p style="margin: 0px;">
+                <a style="color:MediumSeaGreen;font-size:150%;margin: 0px 10px;">▬</a> Posible potabilidad
+            </p>
+            <hr style="margin: 0 auto;">
+            <p style="margin: 0px;">
+                <a style="color:Crimson;font-size:150%;margin: 0px 10px;">▬</a> Malas condiciones
+            </p>
+
+        </div>
+        {% endmacro %}
+        '''
+    )
 
     if lat[0] != 0 and lng[0] != 0:
         loc = [sum(lat) / len(lat), sum(lng) / len(lng)]
 
     else:
-        loc = [4.6311086, -78.8356402]
+        loc = [5.6311086, -78.8356402]
 
     m = fm.Map(location=loc, zoom_start=14)
 
     for i in range(len_after):
 
+        popup = '''
+            <h5 style="text-align: center;"><b>Datos del Tramo</b></h5>
+            
+            <hr style="margin: 4px auto;">
+            <p style="margin: 6px 0;"><b>Fecha/Hora:</b></p>
+            <p style="margin: 6px 0;">{}</p>
+            <hr style="margin: 4px auto;">
+
+            <p style="margin: 6px 0;"><b>Latitud:</b> {}</p>
+            <p style="margin: 6px 0;"><b>Longitud:</b> {}</p>
+            <p style="margin: 6px 0;"><b>Altitud:</b> {} msnm</p>
+
+            <hr style="margin: 4px auto;">
+
+            <div style="display: grid; grid-template-columns: 55px 70px 20px; align-items: center;">
+                <p style="margin: 6px 0;">▸ <b>pH:</b></p> <p style="margin: 6px 0;">&ensp;{}&ensp;</p>  <b>{}</b>
+                <p style="margin: 6px 0;">▸ <b>Cond:</b></p> <p style="margin: 6px 0;">&ensp;{}&ensp;</p>  <b>{}</b>
+                <p style="margin: 6px 0;">▸ <b>OD:</b></p> <p style="margin: 6px 0;">&ensp;{}&ensp;</p>  <b>{}</b>
+                <p style="margin: 6px 0;">▸ <b>Temp:</b></p> <p style="margin: 6px 0;">&ensp;{}&ensp;</p>  <b>{}</b>
+            </div>
+
+            <hr style="margin: 4px auto;">
+
+            <p style="margin: 6px 0; text-align: center;"><b>Resultado:&ensp;{}</p></b>
+        '''.format(
+            df['Timestamp'][i],
+            df['Lat'][i],
+            df['Lng'][i],
+            df['Alt'][i],
+            df['pH'][i],
+            tabVerif['pH'][i],
+            df['K'][i],
+            tabVerif['K'][i],
+            df['OD'][i],
+            tabVerif['OD'][i],
+            df['T'][i],
+            tabVerif['T'][i],
+            tabVerif['Cumplimiento'][i]
+        )
+
         if lat[0] != 0 and lng[0] != 0:
 
             if verif[i] == '✔':
-                colCirc = 'darkgreen'
-                colLine = 'mediumseagreen'
-            else:
-                colCirc = 'darkred'
-                colLine = 'crimson'
+                colCirc = 'DarkGreen'
+                colLine = 'MediumSeaGreen'
+
+            elif verif[i] == 'X':
+                colCirc = 'DarkRed'
+                colLine = 'Crimson'
 
             if i == 0:
-                fm.Marker([lat[0], lng[0]], icon=fm.Icon(color='darkblue'), tooltip='Inicio').add_to(m)
+                fm.Marker((lat[0], lng[0]), icon=fm.Icon(color='black', icon='play', prefix='fa'),
+                          tooltip='Inicio').add_to(m)
 
             elif i == len_after - 1:
 
                 fm.Circle((lat[i], lng[i]), radius=1, color=colCirc).add_to(m)
 
-                fm.PolyLine([[lat[i - 1], lng[i - 1]], [lat[i], lng[i]]], color=colLine).add_to(m)
+                fm.PolyLine([[lat[i - 1], lng[i - 1]], [lat[i], lng[i]]], color=colLine, popup=popup).add_to(m)
 
-                fm.Marker([lat[i], lng[i]], icon=fm.Icon(color='darkblue'), tooltip='Final').add_to(m)
+                fm.Marker([lat[i], lng[i]], icon=fm.Icon(color='black', icon='stop', prefix='fa'),
+                          tooltip='Final').add_to(m)
 
             else:
                 fm.Circle((lat[i], lng[i]), radius=1, color=colCirc).add_to(m)
 
-                fm.PolyLine([[lat[i - 1], lng[i - 1]], [lat[i], lng[i]]], color=colLine).add_to(m)
+                fm.PolyLine([[lat[i - 1], lng[i - 1]], [lat[i], lng[i]]], color=colLine, popup=popup).add_to(m)
 
+    m.get_root().add_child(legend)
     m.save('templates/map.html')
 
     return analysis
--- libs/wordfile.py
+++ libs/wordfile.py
@@ -1,9 +1,10 @@
 import io
+import locale
+
 from docx import Document
 from docx.shared import Cm
 
 from datetime import datetime
-import locale
 
 locale.setlocale(locale.LC_TIME, 'es_CO')
 
@@ -41,28 +42,53 @@ def createReport(report_data, analysis):
 
     # Contenido del Documento
 
+    p = doc.add_paragraph('')
+    p.add_run(str(datetime.now().strftime("%A, %d de %B de %Y"))).bold = True
+
     doc.add_heading(report_data['title'], 1) if report_data['title'] else doc.add_heading(
         'Reporte Preliminar de Calidad del Agua', 0)
 
+    doc.add_heading(
+        'Análisis preliminar - ' + report_data['sourceType'] + ' ' + report_data['sourceName'] + ', ' + report_data[
+            'zoneName'], 2)
+
+    doc.add_paragraph('')
+
     doc.add_paragraph(
-        'Este es un reporte preliminar de la calidad del agua generado en la aplicación web del Sistema Roboat '
-        'Stingray. Aquí se describen las caracteristicas de las variables medidas y los datos estadisticos '
-        'recolectados a partir del análisis.')
+        'Este reporte sobre el análisis preliminar de la calidad del agua es generado en la aplicación web del Sistema Roboat Stingray. Aquí se describen las características de las variables medidas y los datos estadísticos recolectados.')
 
-    p = doc.add_paragraph('\n')
-    p.add_run(str(datetime.now().strftime("%A, %d de %B de %Y")) + '\n\n').bold = True
+    doc.add_paragraph('')
 
+    p = doc.add_paragraph('')
     p.add_run('Autor(es): ').bold = True
-    p.add_run(report_data['author'] + '\n\n') if report_data['author'] else p.add_run('Aquí va el nombre del autor(es)\n\n')
+    p.add_run(report_data['author']) if report_data['author'] else p.add_run('Aquí va el nombre del autor(es)')
+
+    p.add_run('\n\n')
 
     p.add_run('Dirigido a: ').bold = True
-    p.add_run(report_data['sendTo'] + '\n\n') if report_data['sendTo'] else p.add_run('Aquí va la entidad o persona a quien se dirige el reporte\n\n')
+    p.add_run(report_data['sendTo']) if report_data['sendTo'] else p.add_run(
+        'Aquí va la entidad o persona a quien se dirige el reporte')
+
+    p.add_run('\n\n')
 
     p.add_run('Razón del reporte:\n').bold = True
-    p.add_run(report_data['reason'] + '\n\n') if report_data['reason'] else p.add_run('Aquí va la descripción del por qué se genera el reporte\n\n')
+    p.add_run(report_data['reason']) if report_data['reason'] else p.add_run(
+        'Aquí va la descripción del por qué se genera el reporte')
+
+    p.add_run('\n\n')
+
+    if report_data['descrip'] != '':
+        p.add_run('Descripción del lugar:\n').bold = True
+        p.add_run(report_data['descrip']) if report_data['descrip'] else p.add_run(
+            'Aquí va la descripción del lugar de estudio.')
+
+        p.add_run('\n')
+
+    doc.add_heading('Estadísticas de las mediciones:', 2)
 
     # Tabla
     t = doc.add_table(analysis['C'].shape[0] + 1, analysis['C'].shape[1])
+    t.style = 'Colorful Grid Accent 1'
 
     # add the header rows.
     for j in range(analysis['C'].shape[-1]):
@@ -72,6 +98,68 @@ def createReport(report_data, analysis):
     for i in range(analysis['C'].shape[0]):
         for j in range(analysis['C'].shape[-1]):
             t.cell(i + 1, j).text = str(analysis['C'].values[i, j])
+
+    doc.add_paragraph('')
+
+    doc.add_heading('Resumen del análisis:', 2)
+
+    doc.add_paragraph(analysis['A'] + ', de las cuales ' + analysis['B'])
+    doc.add_paragraph(
+        'Hay que recordar que las sondas utilizadas son de marca AtlasScientific, cuyos rangos de medición son:')
+
+    doc.add_paragraph('pH (Lab): desde 0.005 hasta 14.', style='List Bullet')
+    doc.add_paragraph('Conductividad (Lab - K 1.0): desde 5 µS/cm hasta 500000 µS/cm.', style='List Bullet')
+    doc.add_paragraph('Oxígeno disuelto (Lab): desde 0.1 mg/L hasta 100 mg/L.', style='List Bullet')
+    doc.add_paragraph('Temperatura (PT-1000): desde -200ºC hasta 850ºC.', style='List Bullet')
+
+    doc.add_paragraph('Se identificó que el ' + str(
+        analysis['F']['% Cumplimiento Total'][1]) + '% del agua de la fuente hídrica, monitoreada en la ubicación ' +
+                      report_data[
+                          'zoneName'] + ', cumple con las resoluciones ambientales: Resolución 2115 de 2007 y Objetivos de Calidad de Agua - Resolución 3560 de 2015; frente al ' + str(
+        analysis['F']['% Cumplimiento Total'][0]) + '% que no lo hace.')
+
+    doc.add_paragraph('Es posible decir entonces que el ' + str(
+        analysis['F']['% Cumplimiento Total'][1]) + '% del agua, tiene una alta probabilidad de ser potable.')
+
+    doc.add_heading('Tabla de cumplimiento total:', 2)
+    doc.add_paragraph(
+        'Esta tabla muestra el porcentaje de cumplimiento de ambas resoluciones sobre el total de tramos analizados, a partir del recorrido realizado por el sistema Roboat Stingray en la fuente hídrica.')
+
+    t = doc.add_table(analysis['F'].shape[0] + 1, analysis['F'].shape[1])
+    t.style = 'Colorful Grid Accent 1'
+
+    # add the header rows.
+    for j in range(analysis['F'].shape[-1]):
+        t.cell(0, j).text = analysis['F'].columns[j]
+
+    # add the rest of the data frame
+    for i in range(analysis['F'].shape[0]):
+        for j in range(analysis['F'].shape[-1]):
+            t.cell(i + 1, j).text = str(analysis['F'].values[i, j])
+
+    doc.add_paragraph('')
+
+    doc.add_heading('Tabla de cumplimiento por variable:', 2)
+    doc.add_paragraph(
+        'Esta tabla muestra el porcentaje de cumplimiento las resoluciones para cada variable, sobre el total de mediciones analizadas. Hay que tener en cuenta que la Resolución 2115 evalúa el pH y la Conductividad, y que los Objetivos de Calidad de Agua evalúan el oxígeno disuelto; para la temperatura, el rango tolerable se programó entre los 5ºC hasta los 30ºC.')
+
+    t = doc.add_table(analysis['E'].shape[0] + 1, analysis['E'].shape[1])
+    t.style = 'Colorful Grid Accent 1'
+
+    # add the header rows.
+    for j in range(analysis['E'].shape[-1]):
+        t.cell(0, j).text = analysis['E'].columns[j]
+
+    # add the rest of the data frame
+    for i in range(analysis['E'].shape[0]):
+        for j in range(analysis['E'].shape[-1]):
+            t.cell(i + 1, j).text = str(analysis['E'].values[i, j])
+
+    doc.add_paragraph('')
+
+    doc.add_heading('Mapa del recorrido:', 2)
+    doc.add_paragraph(
+        'Inserte aquí su captura de pantalla del recorrido realizado por el catamarán del sistema para añadir más información valiosa a si reporte.')
 
     # Guardado del archivo en memoria
 
