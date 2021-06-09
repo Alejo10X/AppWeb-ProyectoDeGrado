b5ab35b06d181e14108df94e33c5f348b3c4f79a
Alejo Castañeda <facastaneda@uniboyaca.edu.co>
2021-06-08 21:08:14 -0500

    5-N.1. Se reestructura el proyecto para ser utilizado como proyecto extendible de flask. Se mejoran la interfaces de las páginas de inicio, sign up y login.

--- webapp/modules/forms.py
+++ webapp/modules/forms.py
@@ -1,33 +1,34 @@
-from flask_wtf import FlaskForm
-from wtforms import Form, StringField, PasswordField, ValidationError, TextAreaField, SelectField, IntegerField,validators
+from wtforms import Form, StringField, PasswordField, ValidationError, TextAreaField, SelectField, IntegerField, validators
 
 
-def checkEmailArr(form, field):
+def check_email_st(form, field):
     if '@' not in field.data:
         raise ValidationError('El correo debe poseer una arroba (@)')
 
-def checkEmailDot(form, field):
+
+def check_email_dot(form, field):
     if '.' not in field.data:
         raise ValidationError('El correo debe poseer una extensión de dominio como (.com .co .net) etc.')
 
-def checkEmpyTextLength(form, field):
+
+def check_empty_text_length(form, field):
     if field.data != '':
+        e = ''
         if field.name == 'sendTo':
             e = 'El nombre del destinatario'
         elif field.name == 'descript':
             e = 'La descripción del lugar'
 
         if len(field.data) <= 4:
-                raise ValidationError('{} debe poseer al menos 4 caracteres de longitud'.format(e))
+            raise ValidationError('{} debe poseer al menos 4 caracteres de longitud'.format(e))
 
-def checkChoice(form, field):
+
+def check_choice(form, field):
     if field.data == '':
         raise ValidationError('Seleccione una opción válida')
 
 
-
 class SignupForm(Form):
-
     userName = StringField('', [
         validators.DataRequired('Este campo es requerido')
     ])
@@ -39,8 +40,8 @@ class SignupForm(Form):
     emailSign = StringField('', [
         validators.Length(min=6, message='El correo debe poseer al menos 6 caracteres de longitud'),
         validators.DataRequired('Este campo es requerido'),
-        checkEmailArr,
-        checkEmailDot
+        check_email_st,
+        check_email_dot
     ])
 
     passwordSign = PasswordField('', [
@@ -52,13 +53,12 @@ class SignupForm(Form):
     confirm = PasswordField('')
 
 
-
 class LoginForm(Form):
     emailLog = StringField('', [
         validators.Length(min=6, message='El correo debe poseer al menos 6 caracteres de longitud'),
         validators.DataRequired('Este campo es requerido'),
-        checkEmailArr,
-        checkEmailDot
+        check_email_st,
+        check_email_dot
     ])
 
     passwordLog = PasswordField('', [
@@ -67,13 +67,11 @@ class LoginForm(Form):
     ])
 
 
-
 class ChannelForm(Form):
     channel = IntegerField('', [
         validators.NumberRange(min=0, max=9999999, message='El número de ID de la base de datos debe poseer entre 5 y 8 caracteres'),
         validators.DataRequired('Este campo es requerido')
     ])
-
 
 
 class ReportForm(Form):
@@ -88,17 +86,17 @@ class ReportForm(Form):
     ])
 
     sendTo = StringField('', [
-        checkEmpyTextLength
+        check_empty_text_length
     ])
 
     reason = TextAreaField('', [
         validators.DataRequired('Este campo es requerido'),
-        validators.Length(min=4 , message='La razón debe poseer al menos 4 caracteres de longitud')
+        validators.Length(min=4, message='La razón debe poseer al menos 4 caracteres de longitud')
     ])
 
     sourceType = SelectField('', [
         validators.DataRequired('Este campo es requerido'),
-        checkChoice
+        check_choice
     ], choices=[
         ('', 'Seleccione una Opción'),
         ('Arrollo', 'Arrollo'), ('Canal', 'Canal'), ('Charco', 'Charco'), ('Ciénaga', 'Ciénaga'), ('Embalse', 'Embalse'),
@@ -107,15 +105,15 @@ class ReportForm(Form):
     ])
 
     sourceName = StringField('', [
-        validators.Length(min=3 , message='El nombre de la fuente debe poseer al menos 3 caracteres de longitud'),
+        validators.Length(min=3, message='El nombre de la fuente debe poseer al menos 3 caracteres de longitud'),
         validators.DataRequired('Este campo es requerido')
     ])
 
     zoneName = StringField('', [
-        validators.Length(min=4 , message='El nombre/ubicación de la zona debe poseer al menos 4 caracteres de longitud'),
+        validators.Length(min=4, message='El nombre/ubicación de la zona debe poseer al menos 4 caracteres de longitud'),
         validators.DataRequired('Este campo es requerido')
     ])
 
     descript = TextAreaField('', [
-        checkEmpyTextLength
+        check_empty_text_length
     ])
